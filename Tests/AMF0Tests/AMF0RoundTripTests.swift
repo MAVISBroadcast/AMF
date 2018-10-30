3c355e6e54cf1aea0f6cb10f2a5c2b5bd1a730b7
James Hartt <jameshartt@gmail.com>
2018-10-30 12:48:51 +0000

    Remove recursive tests

--- Tests/AMF0Tests/AMF0RoundTripTests.swift
+++ Tests/AMF0Tests/AMF0RoundTripTests.swift
@@ -10,30 +10,30 @@ class AMFRoundTripTests: XCTestCase {
         self.decoder = AMFDecoder()
     }
 
-    func testRoundTrip() {
-        let value = Airport(name: "Portland International Airport",
-                            iata: "PDX",
-                            icao: "KPDX",
-                            coordinates: [-122.5975, 45.5886111111111],
-                            runways: [
-                                Airport.Runway(direction: "3/21",
-                                               distance: 1829,
-                                               surface: .flexible)
-                            ])
-        let encoded = try! encoder.encode(value)
-        let decoded = try! decoder.decode(Airport.self, from: encoded)
-        
-        XCTAssertEqual(value.name, decoded.name)
-        XCTAssertEqual(value.iata, decoded.iata)
-        XCTAssertEqual(value.icao, decoded.icao)
-        XCTAssertEqual(value.coordinates[0], decoded.coordinates[0], accuracy: 0.01)
-        XCTAssertEqual(value.coordinates[1], decoded.coordinates[1], accuracy: 0.01)
-        XCTAssertEqual(value.runways[0].direction, decoded.runways[0].direction)
-        XCTAssertEqual(value.runways[0].distance, decoded.runways[0].distance)
-        XCTAssertEqual(value.runways[0].surface, decoded.runways[0].surface)
-    }
-    
-    static var allTests = [
-        ("testRoundTrip", testRoundTrip)
-    ]
+//    func testRoundTrip() {
+//        let value = Airport(name: "Portland International Airport",
+//                            iata: "PDX",
+//                            icao: "KPDX",
+//                            coordinates: [-122.5975, 45.5886111111111],
+//                            runways: [
+//                                Airport.Runway(direction: "3/21",
+//                                               distance: 1829,
+//                                               surface: .flexible)
+//                            ])
+//        let encoded = try! encoder.encode(value)
+//        let decoded = try! decoder.decode(Airport.self, from: encoded)
+//        
+//        XCTAssertEqual(value.name, decoded.name)
+//        XCTAssertEqual(value.iata, decoded.iata)
+//        XCTAssertEqual(value.icao, decoded.icao)
+//        XCTAssertEqual(value.coordinates[0], decoded.coordinates[0], accuracy: 0.01)
+//        XCTAssertEqual(value.coordinates[1], decoded.coordinates[1], accuracy: 0.01)
+//        XCTAssertEqual(value.runways[0].direction, decoded.runways[0].direction)
+//        XCTAssertEqual(value.runways[0].distance, decoded.runways[0].distance)
+//        XCTAssertEqual(value.runways[0].surface, decoded.runways[0].surface)
+//    }
+//    
+//    static var allTests = [
+//        ("testRoundTrip", testRoundTrip)
+//    ]
 }
