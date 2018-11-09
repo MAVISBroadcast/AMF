347f3f8680b9dcf2b1626884ead96e7279dda540
James Hartt <jameshartt@gmail.com>
2018-11-09 11:43:14 +0000

    Add LinuxMain tests

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,6 +1,13 @@
 import XCTest
-@testable import <#Format#>Tests
+@testable import AMFTests
 
 XCTMain([
-    testCase(<#Format#>Tests.allTests),
+    testCase(AMF0DecodingTests.allTests),
+    testCase(AMF0EncodingTests.allTests),
+    testCase(AMF0PerformanceTests.allTests),
+    testCase(AMF0RoundTripTests.allTests),
+    testCase(AMF3DecodingTests.allTests),
+    testCase(AMF3EncodingTests.allTests),
+    testCase(AMF3PerformanceTests.allTests),
+    testCase(AMF3RoundTripTests.allTests),
 ])
